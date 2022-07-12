ccdda39880a4ce1a985b38e7017f32d6bd6f3773
Nicholas Chen <103069423+noble1241@users.noreply.github.com>
2022-07-11 18:26:46 -0700

    update test

--- src/main/java/frc/robot/systems/teleOp.java
+++ src/main/java/frc/robot/systems/teleOp.java
@@ -9,24 +9,25 @@ public class teleOp {
     /* ======================== Constants ======================== */
     // FSM state definitions
     public enum FSMState {
-        START_STATE,
-        OTHER_STATE
+        MOVE
     }
     private static final float MOTOR_RUN_POWER = 0.1f;
     /* ======================== Private variables ======================== */
     private FSMState currentState;
     // Hardware devices should be owned by one and only one system. They must
     // be private to their owner system and may not be used elsewhere.
-    private CANSparkMax exampleMotor;
+    private CANSparkMax RightMotor, LeftMotor;
     /* ======================== Constructor ======================== */
     /**
      * Create FSMSystem and initialize to starting state. Also perform any
      * one-time initialization or configuration of hardware required. Note
-     * the constructor is called only once when the robot boots.
+     * the constructor is called    only once when the robot boots.
      */
     public teleOp() {
         // Perform hardware init
-        exampleMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_SHOOTER,
+        RightMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_RIGHT,
+                                        CANSparkMax.MotorType.kBrushless);
+        LeftMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_LEFT,
                                         CANSparkMax.MotorType.kBrushless);
         // Reset state machine
         reset();
@@ -48,9 +49,9 @@ public class teleOp {
      * Ex. if the robot is enabled, disabled, then reenabled.
      */
     public void reset() {
-        currentState = FSMState.START_STATE;
+        currentState = FSMState.MOVE;
         // Call one tick of update to ensure outputs reflect start state
-        update(null);
+        //update(null);
     }
     /**
      * Update FSM based on new inputs. This function only calls the FSM state
@@ -60,11 +61,8 @@ public class teleOp {
      */
     public void update(TeleopInput input) {
         switch (currentState) {
-            case START_STATE:
-                handleStartState(input);
-                break;
-            case OTHER_STATE:
-                handleOtherState(input);
+            case MOVE:
+                handle(input);
                 break;
             default:
                 throw new IllegalStateException("Invalid state: " + currentState.toString());
@@ -82,18 +80,7 @@ public class teleOp {
      * @return FSM state for the next iteration
      */
     private FSMState nextState(TeleopInput input) {
-        switch (currentState) {
-            case START_STATE:
-                if (input != null) {
-                    return FSMState.OTHER_STATE;
-                } else {
-                    return FSMState.START_STATE;
-                }
-            case OTHER_STATE:
-                return FSMState.OTHER_STATE;
-            default:
-                throw new IllegalStateException("Invalid state: " + currentState.toString());
-        }
+        return FSMState.MOVE;
     }
     /* ------------------------ FSM state handlers ------------------------ */
     /**
@@ -101,15 +88,13 @@ public class teleOp {
      * @param input Global TeleopInput if robot in teleop mode or null if
      *        the robot is in autonomous mode.
      */
-    private void handleStartState(TeleopInput input) {
-        exampleMotor.set(0);
+    private void handle(TeleopInput input) {
+        RightMotor.set(0.2);
+        LeftMotor.set(0.2);
     }
     /**
      * Handle behavior in OTHER_STATE.
      * @param input Global TeleopInput if robot in teleop mode or null if
      *        the robot is in autonomous mode.
      */
-    private void handleOtherState(TeleopInput input) {
-        exampleMotor.set(MOTOR_RUN_POWER);
-    }
 }
