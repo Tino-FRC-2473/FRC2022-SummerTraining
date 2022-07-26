47ce5a301edc22e88e38f752a14d4e962aaa9a6a
Advaith Nair <advaithnair26@gmail.com>
2022-07-25 18:56:05 -0700

    BALLS !!!

---
+++ src/main/java/frc/robot/systems/BallHandling.java
@@ -0,0 +1,135 @@
+package frc.robot.systems;
+
+// WPILib Imports
+
+// Third party Hardware Imports
+import com.revrobotics.CANSparkMax;
+
+// Robot Imports
+import frc.robot.TeleopInput;
+import frc.robot.HardwareMap;
+
+public class BallHandling {
+	/* ======================== Constants ======================== */
+	// FSM state definitions
+	public enum FSMState {
+		SPIN,
+		IDLE
+	}
+
+	private static final float MOTOR_RUN_POWER = 0.1f;
+
+	/* ======================== Private variables ======================== */
+	private FSMState currentState;
+
+	// Hardware devices should be owned by one and only one system. They must
+	// be private to their owner system and may not be used elsewhere.
+	private CANSparkMax intakeMotor;
+
+	/* ======================== Constructor ======================== */
+	/**
+	 * Create FSMSystem and initialize to starting state. Also perform any
+	 * one-time initialization or configuration of hardware required. Note
+	 * the constructor is called only once when the robot boots.
+	 */
+	public BallHandling() {
+		// Perform hardware init
+		intakeMotor = new CANSparkMax(HardwareMap.INTAKE_MOTOR,
+										CANSparkMax.MotorType.kBrushless);
+
+		// Reset state machine
+		reset();
+	}
+
+	/* ======================== Public methods ======================== */
+	/**
+	 * Return current FSM state.
+	 * @return Current FSM state
+	 */
+	public FSMState getCurrentState() {
+		return currentState;
+	}
+	/**
+	 * Reset this system to its start state. This may be called from mode init
+	 * when the robot is enabled.
+	 *
+	 * Note this is distinct from the one-time initialization in the constructor
+	 * as it may be called multiple times in a boot cycle,
+	 * Ex. if the robot is enabled, disabled, then reenabled.
+	 */
+	public void reset() {
+		currentState = FSMState.IDLE;
+
+		// Call one tick of update to ensure outputs reflect start state
+		update(null);
+	}
+	/**
+	 * Update FSM based on new inputs. This function only calls the FSM state
+	 * specific handlers.
+	 * @param input Global TeleopInput if robot in teleop mode or null if
+	 *        the robot is in autonomous mode.
+	 */
+	public void update(TeleopInput input) {
+		switch (currentState) {
+			case SPIN:
+				handleSpinState(input);
+				break;
+
+			case IDLE:
+				handleIdleState(input);
+				break;
+
+			default:
+				throw new IllegalStateException("Invalid state: " + currentState.toString());
+		}
+		currentState = nextState(input);
+	}
+
+	/* ======================== Private methods ======================== */
+	/**
+	 * Decide the next state to transition to. This is a function of the inputs
+	 * and the current state of this FSM. This method should not have any side
+	 * effects on outputs. In other words, this method should only read or get
+	 * values to decide what state to go to.
+	 * @param input Global TeleopInput if robot in teleop mode or null if
+	 *        the robot is in autonomous mode.
+	 * @return FSM state for the next iteration
+	 */
+	private FSMState nextState(TeleopInput input) {
+		switch (currentState) {
+			case SPIN:
+				if (!input.isIntakeButtonPressed()) {
+					return FSMState.IDLE;
+				} else {
+					return FSMState.SPIN;
+				}
+
+			case IDLE:
+				if (input.isIntakeButtonPressed()) {
+					return FSMState.SPIN;
+				} else {
+					return FSMState.IDLE;
+				}
+			default:
+				throw new IllegalStateException("Invalid state: " + currentState.toString());
+		}
+	}
+
+	/* ------------------------ FSM state handlers ------------------------ */
+	/**
+	 * Handle behavior in START_STATE.
+	 * @param input Global TeleopInput if robot in teleop mode or null if
+	 *        the robot is in autonomous mode.
+	 */
+	private void handleSpinState(TeleopInput input) {
+		intakeMotor.set(MOTOR_RUN_POWER);
+	}
+	/**
+	 * Handle behavior in OTHER_STATE.
+	 * @param input Global TeleopInput if robot in teleop mode or null if
+	 *        the robot is in autonomous mode.
+	 */
+	private void handleIdleState(TeleopInput input) {
+		intakeMotor.set(0);
+	}
+}
