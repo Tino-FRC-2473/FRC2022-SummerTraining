b44bfd316f90dad7ee3ea1da7f24a20ea1d594ed
AknA13 <akshaya91306@gmail.com>
2022-07-26 23:47:18 -0700

    omni-directional strafe + center point rotation

--- src/main/java/frc/robot/systems/FSMSystem.java
+++ src/main/java/frc/robot/systems/FSMSystem.java
@@ -1,5 +1,4 @@
 package frc.robot.systems;
-
 // WPILib Imports
 
 // Third party Hardware Imports
@@ -13,18 +12,20 @@ public class FSMSystem {
 	/* ======================== Constants ======================== */
 	// FSM state definitions
 	public enum FSMState {
-		START_STATE,
-		OTHER_STATE
+		TeleOP
 	}
 
 	private static final float MOTOR_RUN_POWER = 0.1f;
-
+	private static final double PI_OVER_FOUR = Math.PI / 4;
 	/* ======================== Private variables ======================== */
 	private FSMState currentState;
 
 	// Hardware devices should be owned by one and only one system. They must
 	// be private to their owner system and may not be used elsewhere.
-	private CANSparkMax exampleMotor;
+	private CANSparkMax frontLeft;
+	private CANSparkMax frontRight;
+	private CANSparkMax backLeft;
+	private CANSparkMax backRight;
 
 	/* ======================== Constructor ======================== */
 	/**
@@ -34,9 +35,14 @@ public class FSMSystem {
 	 */
 	public FSMSystem() {
 		// Perform hardware init
-		exampleMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_SHOOTER,
+		frontLeft = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_LEFT,
 										CANSparkMax.MotorType.kBrushless);
-
+		frontRight = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_RIGHT,
+										CANSparkMax.MotorType.kBrushless);
+		backLeft = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_BACK_LEFT,
+										CANSparkMax.MotorType.kBrushless);
+		backRight = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_BACK_RIGHT,
+										CANSparkMax.MotorType.kBrushless);
 		// Reset state machine
 		reset();
 	}
@@ -58,7 +64,7 @@ public class FSMSystem {
 	 * Ex. if the robot is enabled, disabled, then reenabled.
 	 */
 	public void reset() {
-		currentState = FSMState.START_STATE;
+		currentState = FSMState.TeleOP;
 
 		// Call one tick of update to ensure outputs reflect start state
 		update(null);
@@ -71,14 +77,9 @@ public class FSMSystem {
 	 */
 	public void update(TeleopInput input) {
 		switch (currentState) {
-			case START_STATE:
-				handleStartState(input);
+			case TeleOP:
+				handleTeleOP(input);
 				break;
-
-			case OTHER_STATE:
-				handleOtherState(input);
-				break;
-
 			default:
 				throw new IllegalStateException("Invalid state: " + currentState.toString());
 		}
@@ -95,21 +96,10 @@ public class FSMSystem {
 	 *        the robot is in autonomous mode.
 	 * @return FSM state for the next iteration
 	 */
+	//left joystick:strafe
+	//right is turn
 	private FSMState nextState(TeleopInput input) {
-		switch (currentState) {
-			case START_STATE:
-				if (input != null) {
-					return FSMState.OTHER_STATE;
-				} else {
-					return FSMState.START_STATE;
-				}
-
-			case OTHER_STATE:
-				return FSMState.OTHER_STATE;
-
-			default:
-				throw new IllegalStateException("Invalid state: " + currentState.toString());
-		}
+		return FSMState.TeleOP;
 	}
 
 	/* ------------------------ FSM state handlers ------------------------ */
@@ -118,15 +108,33 @@ public class FSMSystem {
 	 * @param input Global TeleopInput if robot in teleop mode or null if
 	 *        the robot is in autonomous mode.
 	 */
-	private void handleStartState(TeleopInput input) {
-		exampleMotor.set(0);
-	}
-	/**
-	 * Handle behavior in OTHER_STATE.
-	 * @param input Global TeleopInput if robot in teleop mode or null if
-	 *        the robot is in autonomous mode.
-	 */
-	private void handleOtherState(TeleopInput input) {
-		exampleMotor.set(MOTOR_RUN_POWER);
+	private void handleTeleOP(TeleopInput input) {
+		//FLBR funct: y = sin(theta+pi/4)
+		//FRBL funct: y = sin(theta-pi/4)
+		if (input == null) {
+			return;
+		}
+		if (input.getRightJoystickX() == 0) {
+			//omni-directional strafe
+			double theta = Math.atan2(input.getLeftJoystickY(), input.getLeftJoystickX());
+			frontLeft.set(MOTOR_RUN_POWER * Math.sin(theta + (PI_OVER_FOUR)));
+			frontRight.set(-(MOTOR_RUN_POWER * Math.sin(theta - (PI_OVER_FOUR))));
+			backLeft.set(MOTOR_RUN_POWER * Math.sin(theta - (PI_OVER_FOUR)));
+			backRight.set(-(MOTOR_RUN_POWER * Math.sin(theta + (PI_OVER_FOUR))));
+		} else {
+			if (input.getRightJoystickX() > 0) {
+				//cw
+				frontLeft.set(MOTOR_RUN_POWER);
+				frontRight.set(MOTOR_RUN_POWER);
+				backLeft.set(MOTOR_RUN_POWER);
+				backRight.set(MOTOR_RUN_POWER);
+			} else {
+				//ccw
+				frontLeft.set(-MOTOR_RUN_POWER);
+				frontRight.set(-MOTOR_RUN_POWER);
+				backLeft.set(-MOTOR_RUN_POWER);
+				backRight.set(-MOTOR_RUN_POWER);
+			}
+		}
 	}
 }
