c9887824a86699ddf8758a2a7c7c68c0fcf54d0e
adnx26 <advaithnair26@gmail.com>
2022-08-01 18:00:04 -0700

    PORTs

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -6,7 +6,7 @@ package frc.robot;
  */
 public final class HardwareMap {
 	// ID numbers for devices on the CAN bus
-	public static final int INTAKE_MOTOR = 1;
+	public static final int INTAKE_MOTOR = 10;
 	public static final int CAN_ID_SPARK_DRIVE_BACK_RIGHT = 2;
 	public static final int CAN_ID_SPARK_DRIVE_FRONT_LEFT = 3;
 	public static final int CAN_ID_SPARK_DRIVE_BACK_LEFT = 4;
@@ -14,5 +14,5 @@ public final class HardwareMap {
 
 	// Pneumatics channel numbers
 	public static final int PCM_CHANNEL_INTAKE_CYLINDER_EXTEND = 1;
-	public static final int PCM_CHANNEL_INTAKE_CYLINDER_RETRACT = 2;
+	public static final int PCM_CHANNEL_INTAKE_CYLINDER_RETRACT = 0;
 }
