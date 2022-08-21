b13868c2c3f8bc47abb2cefcbf4a99cc5d72dba0
Rajvi Shroff <55897479+r639@users.noreply.github.com>
2022-08-20 18:01:40 -0700

    Update Main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,27 +1,89 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotBase;
+// WPILib Imports
+import edu.wpi.first.wpilibj.TimedRobot;
+
+// Systems
+import frc.robot.systems.FSMSystem;
 
 /**
- * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
- * you are doing, do not modify this file except to change the parameter class to the startRobot
- * call.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation.
  */
-public final class Main {
-  private Main() {
-  	
-  }
+public class Robot extends TimedRobot {
+	private TeleopInput input;
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+	// Systems
+	private FSMSystem fsmSystem;
+
+	/**
+	 * This function is run when the robot is first started up and should be used for any
+	 * initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		System.out.println("robotInit");
+		input = new TeleopInput();
+
+		// Instantiate all systems here
+		fsmSystem = new FSMSystem();
+	}
+
+	@Override
+	public void autonomousInit() {
+		System.out.println("-------- Autonomous Init --------");
+		fsmSystem.reset();
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		fsmSystem.update(input);
+	}
+
+	@Override
+	public void teleopInit() {
+		System.out.println("-------- Teleop Init --------");
+		fsmSystem.reset();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		fsmSystem.update(input);
+	}
+
+	@Override
+	public void disabledInit() {
+		System.out.println("-------- Disabled Init --------");
+	}
+
+	@Override
+	public void disabledPeriodic() {
+
+	}
+
+	@Override
+	public void testInit() {
+		System.out.println("-------- Test Init --------");
+	}
+
+	@Override
+	public void testPeriodic() {
+
+	}
+
+	/* Simulation mode handlers, only used for simulation testing  */
+	@Override
+	public void simulationInit() {
+		System.out.println("-------- Simulation Init --------");
+	}
+
+	@Override
+	public void simulationPeriodic() { }
+
+	// Do not use robotPeriodic. Use mode specific periodic methods instead.
+	@Override
+	public void robotPeriodic() { }
 }
